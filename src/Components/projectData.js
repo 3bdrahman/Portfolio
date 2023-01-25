d7b278f289e7973f73e5a7f09c50c0a7b03f907e
3bdrahman <abdelrahmanyosef07@gmail.com>
2023-01-24 22:19:40 -0500

    add project links

--- src/Components/projectData.js
+++ src/Components/projectData.js
@@ -11,28 +11,28 @@ export const projectData = [
 		title: 'Amazon Clone',
 		description:
 			'An Amazon clone that uses React hooks, Firebase Authentication, Firestore, Stripe API.',
-		link: 'https://airbnb-clone-3bdrahman.vercel.app/',
+		link: 'https://clozon-fc92d.web.app/',
 		image: '../imgs/Amazon.png',
 	},
 	{
 		title: '2-D Self-driving car',
 		description:
 			'self-driving 2-d car with a multilayer neural network and visualizer. Collision detection, Parallelization, and mutation were the main milestones.',
-		link: 'https://airbnb-clone-3bdrahman.vercel.app/',
+		link: 'https://github.com/3bdrahman/Self-driving-car',
 		image: '../imgs/car.png',
 	},
 	{
 		title: 'Mini PDF-viewer',
 		description:
 			'React component that can process a list of PDFs and allow user to select and view PDF inside-app.',
-		link: 'https://airbnb-clone-3bdrahman.vercel.app/',
+		link: 'https://codesandbox.io/s/react-pdf-viewer-goyyz',
 		image: '../imgs/Scrapper__proj.png',
 	},
 	{
 		title: 'Chat-gpt clone (current)',
 		description:
 			'React application that allows user to make requests to OpenAI with a specific model. ',
-		link: 'https://airbnb-clone-3bdrahman.vercel.app/',
+		link: 'https://github.com/3bdrahman/chatGPT-Clone',
 		image: '../imgs/chatGPT.png',
 	},
 	{
@@ -40,7 +40,7 @@ export const projectData = [
 		description:
 			'React application that shows the sorting process of (Bubble, Insertion, Seclection, Merge, Quick) sort algorithms',
 		techs: 'React, NextJS, Tailwind Css, Mapbox API',
-		link: 'https://airbnb-clone-3bdrahman.vercel.app/',
+		link: 'https://github.com/3bdrahman/SortingVisualizer',
 		image: '../imgs/Sorting.png',
 	},
 ];
